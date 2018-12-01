96444dc8e7bea9b1d1972605e44289585fbc313d
Shawei辅助开发 <45140834+shawei0517@users.noreply.github.com>
2018-12-01 10:44:35 +0800

    更新命令

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-ftparchive packages ./ > ./Packages;sed -i -e '/^SHA/d' ./Packages;bzip2 -c9k ./Packages > ./Packages.bz2;printf "Origin: 力天百汇备用\nLabel: 力天百汇备用\nSuite: stable\nVersion: 1.0\nCodename: ltbh\nArchitecture: iphoneos-arm\nComponents: main\nDescription: 力天百汇定制源\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;exit 0;
+apt-ftparchive packages ./ > ./Packages;sed -i -e '/^SHA/d' ./Packages;bzip2 -c9k ./Packages > ./Packages.bz2;rm -f ./Packages;exit 0;
